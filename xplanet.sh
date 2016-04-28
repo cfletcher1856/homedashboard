351e2399d5b3e1a38517b6f80e21de4892222860
Colin Fletcher <cfletcher1856@gmail.com>
2016-04-28 12:46:59 -0500

    fixing things

--- xplanet.sh
+++ xplanet.sh
@@ -1,13 +1,11 @@
 #! /bin/sh
 ### BEGIN INIT INFO
 # Provides:          xplanet
-# Required-Start:
+# Required-Start:    $all
 # Required-Stop:
-# Should-Stop:
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
 # Short-Description: xplanet
-# Description:       xplanet
 ### END INIT INFO
 
 
