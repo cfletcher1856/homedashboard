3a65194a919544e17662127ee5059694d7b0fa2e
Colin Fletcher <cfletcher1856@gmail.com>
2016-04-29 10:15:05 -0500

    Updating xplanet to run on a crontab

--- xplanet.sh
+++ xplanet.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-xplanet -conf ~/Projects/homedashboard/xplanet.conf -output ~/Projects/homedashboard/images/xplanet-earth.png -num_times 1 -body earth -latitude 31 -longitude -98 -geometry 500x500 > /dev/null 2>&1 &
-xplanet -conf ~/Projects/homedashboard/xplanet.conf -output ~/Projects/homedashboard/images/xplanet-moon.png -num_times 1 -body moon -geometry 250x250 > /dev/null 2>&1 &
+xplanet -conf /home/cfletcher/Projects/homedashboard/xplanet.conf -output ~/Projects/homedashboard/images/xplanet-earth.png -num_times 1 -body earth -latitude 31 -longitude -98 -geometry 500x500 > /dev/null 2>&1 &
+xplanet -conf /home/cfletcher/Projects/homedashboard/xplanet.conf -output ~/Projects/homedashboard/images/xplanet-moon.png -num_times 1 -body moon -geometry 250x250 > /dev/null 2>&1 &
