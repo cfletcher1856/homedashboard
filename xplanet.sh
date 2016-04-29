cd0e8896ecdda8bcc5617ca760b6ce0c5c620b2b
Colin Fletcher <cfletcher1856@gmail.com>
2016-04-29 10:14:10 -0500

    Updating xplanet to run on a crontab

--- xplanet.sh
+++ xplanet.sh
@@ -1,12 +1,4 @@
 #! /bin/sh
-### BEGIN INIT INFO
-# Provides:          xplanet
-# Required-Start:    $all
-# Required-Stop:
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: xplanet
-### END INIT INFO
 
-xplanet -conf ~/Projects/homedashboard/xplanet.conf -output ~/Projects/homedashboard/images/xplanet-earth.png -wait 120 -body earth -latitude 31 -longitude -98 -geometry 500x500 > /dev/null 2>&1 &
-xplanet -conf ~/Projects/homedashboard/xplanet.conf -output ~/Projects/homedashboard/images/xplanet-moon.png -wait 600 -body moon -geometry 250x250 > /dev/null 2>&1 &
+xplanet -conf ~/Projects/homedashboard/xplanet.conf -output ~/Projects/homedashboard/images/xplanet-earth.png -num_times 1 -body earth -latitude 31 -longitude -98 -geometry 500x500 > /dev/null 2>&1 &
+xplanet -conf ~/Projects/homedashboard/xplanet.conf -output ~/Projects/homedashboard/images/xplanet-moon.png -num_times 1 -body moon -geometry 250x250 > /dev/null 2>&1 &
